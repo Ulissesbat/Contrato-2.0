31d3385aa921418507a01b273f12849834f806c6
Ulisses Batista Santana <santana.gru@gmail.com>
2025-08-22 21:19:20 -0300

    package.json

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
+      contextIsolation: false,
+      enableRemoteModule: true
+    },
+    icon: path.join(__dirname, 'icon.ico'), // opcional
+    show: false // para evitar flicker no carregamento
   });
 
   mainWindow.loadFile('index.html');
+  
+  // Mostrar a janela quando estiver pronta
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
 }
 
 app.whenReady().then(createWindow);
